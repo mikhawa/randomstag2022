83e12a7dec23e9d6dfff2b5fd1693302f8484db2
mikhawa <michaeljpitz@gmail.com>
2022-09-08 15:04:38 +0200

    publicController

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
+session_start();
 
 require_once "config.php";
+
+// Personal autoload
+spl_autoload_register(function ($class) {
+    include_once 'model/' . str_replace('\\', '/', $class) . '.php';
+});
+
+// connect with MyPDO
+try {
+    $connectMyPDO = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_HOST . ';charset=' . DB_CHARSET . ';port=' . DB_PORT, DB_LOGIN, DB_PWD);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+
+if(isset($_SESSION['myidsession'])&& $_SESSION['myidsession'] == session_id()){
+
+}else{
+    require "controller/publicController.php";
+}
