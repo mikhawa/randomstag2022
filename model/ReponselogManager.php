59d867b9587796f6879ac530ea9f6f699c92c64c
mikhawa <michaeljpitz@gmail.com>
2022-11-21 16:20:05 +0100

    Reponse pagination methods

--- model/ReponselogManager.php
+++ model/ReponselogManager.php
@@ -35,4 +35,51 @@ class ReponselogManager implements ManagerInterface
         }
     }
 
+    public function countAllLogsByAnnee(int $annee): int|string {
+        $query = $this->connect->prepare("SELECT count(r.idreponseslog) AS nb
+        FROM reponseslog r
+        WHERE r.annee_idannee = ? ;");
+
+        try{
+            $query->execute([$annee]);
+            if($query->rowCount()==0){
+                throw new Exception("Année non existante");
+            }
+            $result = $query->fetch(PDO::FETCH_ASSOC);
+            return (int) $result['nb'];
+
+        }catch(Exception $exception){
+            return $exception->getMessage();
+        }
+    }
+
+    public function selectAllLogsByAnneeWithPG(int $annee, int $page=1, int $perPage=100): array|string{
+
+        // début
+        $debut = ($page-1)*$perPage;
+
+        $prepare = $this->connect->prepare("
+        SELECT r.idreponseslog, r.reponseslogcol, r.remarque, r.reponseslogdate, u.username, s.nom, s.prenom
+        FROM reponseslog r 
+        INNER JOIN user u 
+            ON r.user_iduser = u.iduser
+        INNER JOIN stagiaires s
+            ON s.idstagiaires = r.stagiaires_idstagiaires
+        WHERE r.annee_idannee = ?
+        ORDER BY r.reponseslogdate DESC
+        LIMIT ?,?        
+;
+        ");
+
+        try{
+            $prepare->execute([$annee,$debut,$perPage]);
+            if($prepare->rowCount()==0){
+                throw new Exception("Année non existante");
+            }
+            return $prepare->fetchAll(PDO::FETCH_ASSOC);
+        }catch(Exception $exception){
+            return $exception->getMessage();
+        }
+    }
+
 }
