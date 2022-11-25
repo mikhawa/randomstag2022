af4432968e23d2be571c705c7fe5d0204916a14f
mikhawa <michaeljpitz@gmail.com>
2022-11-25 15:41:20 +0100

    ../controller/timeslotController.php in index.php

---
+++ controller/timeslotController.php
@@ -0,0 +1,41 @@
+<?php
+// management of the display time slot
+if (isset($_GET['temps'])) {
+    switch ($_GET['temps']) {
+        case "tous":
+            // formats the date as datetime in the present and in the past
+            $tps = Calcul::laDate(CHOIX_DATE["tous"]);
+            $letps = "le début";
+            break;
+        case "1-an":
+            $tps = Calcul::laDate(CHOIX_DATE["1 an"]);
+            $letps = "1 an";
+            break;
+        case "6-mois":
+            $tps = Calcul::laDate(CHOIX_DATE["6 mois"]);
+            $letps = "6 mois";
+            break;
+        case "3-mois":
+            $tps = Calcul::laDate(CHOIX_DATE["3 mois"]);
+            $letps = "3 mois";
+            break;
+        case "1-mois":
+            $tps = Calcul::laDate(CHOIX_DATE["1 mois"]);
+            $letps = "1 mois";
+            break;
+        case "2-semaines":
+            $tps = Calcul::laDate(CHOIX_DATE["2 semaines"]);
+            $letps = "2 semaines";
+            break;
+        case "1-semaine":
+            $tps = Calcul::laDate(CHOIX_DATE["1 semaine"]);
+            $letps = "1 semaine";
+            break;
+        default:
+            $tps = Calcul::laDate(CHOIX_DATE["tous"]);
+            $letps = "le début";
+    }
+} else {
+    $tps = Calcul::laDate(CHOIX_DATE["tous"]);
+    $letps = "le début";
+}
--- public/index.php
+++ public/index.php
@@ -36,46 +36,8 @@ try {
 // if you are logged in validly
 if (isset($_SESSION['myidsession']) && $_SESSION['myidsession'] == session_id()) {
 
-    // management of the display time slot
-    if (isset($_GET['temps'])) {
-        switch ($_GET['temps']) {
-            case "tous":
-                // formats the date as datetime in the present and in the past
-                $tps = Calcul::laDate(CHOIX_DATE["tous"]);
-                $letps = "le début";
-                break;
-            case "1-an":
-                $tps = Calcul::laDate(CHOIX_DATE["1 an"]);
-                $letps = "1 an";
-                break;
-            case "6-mois":
-                $tps = Calcul::laDate(CHOIX_DATE["6 mois"]);
-                $letps = "6 mois";
-                break;
-            case "3-mois":
-                $tps = Calcul::laDate(CHOIX_DATE["3 mois"]);
-                $letps = "3 mois";
-                break;
-            case "1-mois":
-                $tps = Calcul::laDate(CHOIX_DATE["1 mois"]);
-                $letps = "1 mois";
-                break;
-            case "2-semaines":
-                $tps = Calcul::laDate(CHOIX_DATE["2 semaines"]);
-                $letps = "2 semaines";
-                break;
-            case "1-semaine":
-                $tps = Calcul::laDate(CHOIX_DATE["1 semaine"]);
-                $letps = "1 semaine";
-                break;
-            default:
-                $tps = Calcul::laDate(CHOIX_DATE["tous"]);
-                $letps = "le début";
-        }
-    } else {
-        $tps = Calcul::laDate(CHOIX_DATE["tous"]);
-        $letps = "le début";
-    }
+    // timeslot choice
+    include_once "../controller/timeslotController.php";
 
     // if we are a student
     if($_SESSION['perm']==0){
